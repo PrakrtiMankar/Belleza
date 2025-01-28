63598b809d17b044832c4c736b6cfa34b6e28664
PrakrtiMankar <prakrtimankar@gmail.com>
2025-01-28 10:59:25 +0530

    updates

--- scripts/baseUrl.js
+++ scripts/baseUrl.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'https://unmarred-blue-delivery.glitch.me';
-
+// export const baseUrl = 'https://unmarred-blue-delivery.glitch.me';
+export const baseUrl = 'https://heady-pickle-lightyear.glitch.me';
 // export const baseUrl = 'http://localhost:3000';
